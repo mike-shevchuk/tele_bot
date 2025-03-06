4665f609a3520afa44dc9625a42e1e8662a6a48f
Mike_Shevchuk <tetraedrgo@gmail.com>
2025-03-06 21:25:19 +0200

    add MiddleWare to share object

--- src/MiddleWare.py
+++ src/MiddleWare.py
@@ -4,17 +4,21 @@ from aiogram.types import TelegramObject, Message
 from typing import Callable, Dict, Any, Awaitable
 
 class SharedContextMiddleware(BaseMiddleware):
-    def __init__(self, logger, foo: int, bar: str):
+    def __init__(self, logger, foo: int, bar: str, bot_func, user_data):
         super().__init__()
         self.logger = logger
+        self.bot_func = bot_func
+        self.user_data = user_data
         self.foo = foo
         self.bar = bar
 
     async def __call__(self, handler, event: TelegramObject, data: Dict[str, Any]) -> Any:
         # Add shared context to data
         data['logger'] = self.logger
+        data['bot_func'] = self.bot_func
         data['foo'] = self.foo
         data['bar'] = self.bar
+        data['user_data'] = self.user_data
 
         # Log the event
         # self.logger.info(f"Handling event: {event}")
