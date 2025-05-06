e509bd63abc0709809df8b45182fdf9e7a27baa4
Ostap <ostap5577@gmail.com>
2025-05-06 16:27:04 +0300

    hotfix

--- src/bot.py
+++ src/bot.py
@@ -71,8 +71,10 @@ class Bot_Func:
             await user_msg.reply(f"An error occurred: {e}")
             return
 
-
-        loc_match = glob.glob(os.path.join('.', f'{loc_video}*'))
+        print(loc_video)
+        pattern = glob.escape(loc_video) + '*'
+        loc_match = glob.glob(os.path.join('.', pattern))
+        print(loc_match)
 
         loc_video  = loc_match[0]
         # await info_wait_button.delete()
