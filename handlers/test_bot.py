686a0b23797009b9371e10ce2b780b4775e47a1c
Bob <bob-shevchuk@gmail.com>
2026-04-02 15:28:04 +0000

    make chels command available only admin or vip

--- handlers/test_bot.py
+++ handlers/test_bot.py
@@ -46,6 +46,15 @@ router = Router()
 #     await inline_query.answer(results=[article])
 #     ...
 
+async def check_access(message: types.Message, allowed_levels: list[Level]):
+    user_bot = message.from_user
+    user_df = ut.get_user_by_id(user_bot.id)
+    user = ut.pandas2pydentic(user_df)
+    if user.level not in allowed_levels:
+        await message.reply('Вибачте, але у вас нема доступу до цієї команди')
+        return None
+    return user
+
 @router.inline_query()
 async def inline_query_handler(inline_query: types.InlineQuery, logger):
     query = inline_query.query.strip()
@@ -166,8 +175,8 @@ async def _load_users_df(message: types.Message, logger, cfg):
 
     df_display = df_user.rename(columns={'id': 'ID', 'username': 'nick', 'full_name': 'name', 'level': 'level_memory'})
     level_to_bytes = lambda x: Level.__members__.get((x).split('.')[-1]).value
-    name4bytes = lambda x: str(Level.__members__.get((x).split('.')[-1])).split('.')[-1]
-    df_display['level'] = df_display['level_memory'].map(name4bytes)
+    get_level_name = lambda x: str(Level.__members__.get((x).split('.')[-1])).split('.')[-1]
+    df_display['level'] = df_display['level_memory'].map(get_level_name)
     df_display['level_memory'] = df_display['level_memory'].map(level_to_bytes)
     df_display[['nick', 'name']] = df_display[['nick', 'name']].fillna('-')
     df_display['nick/name'] = df_display['nick'].str[:13] + '/' + df_display['name'].str[:7]
@@ -185,19 +194,15 @@ async def _reply_users_table(df_display, col: str, message: types.Message, logge
 
 @router.message(Command("chels"))
 async def cmd_show_users_pct(message: types.Message, logger, cfg):
-    user_bot = message.from_user
-    user_df = ut.get_user_by_id(user_bot.id)
-    user = ut.pandas2pydentic(user_df)
+    user = await check_access(message, [Level.admin, Level.vip])
     try:
         df_display = await _load_users_df(message, logger, cfg)
         if df_display is None:
             return
         df_display['size%'] = round(df_display['use_memory'] / df_display['level_memory'] * 100, 1)
-        logger.info(f'>>>>>>>>>>>>{user.level}')
-        if user.level == Level.vip or user.level == Level.admin:
-            await _reply_users_table(df_display, 'size%', message, logger)
-        else:
-            await message.reply('Вибачте, але у вас нема доступу до цієї команди')    
+        if not user:
+            return
+        await _reply_users_table(df_display, 'size%', message, logger)
     except Exception as e:
         logger.exception(f"Помилка при зчитуванні користувачів: {e}")
         await message.reply("❌ Виникла помилка при зчитуванні таблиці користувачів.")
