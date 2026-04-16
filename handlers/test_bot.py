846746fc609534c624e161ce99ab1e95c635f299
Bob <bob-shevchuk@gmail.com>
2026-04-16 18:19:17 +0300

    Fixed another possible errors

--- handlers/test_bot.py
+++ handlers/test_bot.py
@@ -382,7 +382,7 @@ async def cmd_setlevel(message: types.Message, logger, bot: Bot):
         
         target_user.level = new_level
         ut.update_row(target_user)
-        await bot.send_message(target_id, f'Твій новий level: {target_user.level}.')
+        await bot.send_message(target_id, f'Твій новий level: {target_user.level.name}.')
         await message.reply(f'✅ Рівень користувача {target_user.full_name} змінено на {target_user.level.name}')
 
     except Exception as e:
